b7c0f9ecf34e81742abc9a655147dfaa68e06599
Philipp Trunschke <trunschk@math.hu-berlin.de>
2018-07-30 19:42:44 +0200

    retab

--- include/xerus/blockTT.h
+++ include/xerus/blockTT.h
@@ -1,20 +1,20 @@
 // Xerus - A General Purpose Tensor Library
-// Copyright (C) 2014-2018 Benjamin Huber and Sebastian Wolf. 
-// 
+// Copyright (C) 2014-2018 Benjamin Huber and Sebastian Wolf.
+//
 // Xerus is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License,
 // or (at your option) any later version.
-// 
+//
 // Xerus is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with Xerus. If not, see <http://www.gnu.org/licenses/>.
 //
-// For further information on Xerus visit https://libXerus.org 
+// For further information on Xerus visit https://libXerus.org
 // or contact us at contact@libXerus.org.
 
 /**
@@ -29,113 +29,113 @@
 #include "ttNetwork.h"
 
 namespace xerus { namespace internal {
-	/**
-	 * @brief Specialized TensorNetwork class used to represent a BlockTT
-	 */
-	class BlockTT final  {
-	private:
-		static const Index left, right, ext, p, r1, r2;
-		
-	public:
+    /**
+     * @brief Specialized TensorNetwork class used to represent a BlockTT
+     */
+    class BlockTT final  {
+    private:
+        static const Index left, right, ext, p, r1, r2;
+
+    public:
         size_t P;
-		
-		/**
-		 * @brief The position of the core.
-		 * @details CorePosition gives the position of the block/core tensor. All components
-		 * with smaller index are then left-orthogonalized and all components with larger index right-orthogonalized.
-		 */
-		size_t corePosition;
-		size_t blockPosition;
-        
+
+        /**
+         * @brief The position of the core.
+         * @details CorePosition gives the position of the block/core tensor. All components
+         * with smaller index are then left-orthogonalized and all components with larger index right-orthogonalized.
+         */
+        size_t corePosition;
+        size_t blockPosition;
+
         std::vector<Tensor> components;
         std::vector<size_t> dimensions;
-        
-		
-		/*- - - - - - - - - - - - - - - - - - - - - - - - - - Constructors - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-		/** 
-		 * @brief BlockTTs can be default construced.
-		 */
-		BlockTT() = default;
+
+
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - Constructors - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+        /**
+         * @brief BlockTTs can be default construced.
+         */
+        BlockTT() = default;
 
         ///@brief Constructs an zero initialized TTNetwork with the given dimensions, ranks and block dimensions.
         BlockTT(const std::vector<size_t>& _dimensions, const std::vector<size_t>& _ranks, const size_t _blockPosition, const size_t _blockDim);
-		
-		
-		///@brief BlockTTs are default copy constructable.
-		BlockTT(const BlockTT & _cpy) = default;
-		
-		
-		///@brief BlockTTs are default move constructable.
-		BlockTT(      BlockTT&& _mov) noexcept = default;
-		
-		
-		/** 
-		 * @brief Constructs a BlockTT from the given TTTensor.
-		 */
-		explicit BlockTT(const TTTensor& _tttensor, const size_t _blockPosition, const size_t _blockDim);
-		
-		
-		
-		/*- - - - - - - - - - - - - - - - - - - - - - - - - - Standard Operators - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-		///@brief BlockTTs are default assignable.
-		BlockTT& operator=(const BlockTT&  _other) = default;
-		
-		
-		///@brief BlockTTs are default move-assignable.
-		BlockTT& operator=(      BlockTT&& _other) = default;
-		
-		
-		/*- - - - - - - - - - - - - - - - - - - - - - - - - - Miscellaneous - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-		public:
-			
+
+
+        ///@brief BlockTTs are default copy constructable.
+        BlockTT(const BlockTT & _cpy) = default;
+
+
+        ///@brief BlockTTs are default move constructable.
+        BlockTT(      BlockTT&& _mov) noexcept = default;
+
+
+        /**
+         * @brief Constructs a BlockTT from the given TTTensor.
+         */
+        explicit BlockTT(const TTTensor& _tttensor, const size_t _blockPosition, const size_t _blockDim);
+
+
+
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - Standard Operators - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+        ///@brief BlockTTs are default assignable.
+        BlockTT& operator=(const BlockTT&  _other) = default;
+
+
+        ///@brief BlockTTs are default move-assignable.
+        BlockTT& operator=(      BlockTT&& _other) = default;
+
+
+        /*- - - - - - - - - - - - - - - - - - - - - - - - - - Miscellaneous - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+        public:
+
             size_t degree() const;
-            
-            
-			Tensor& component(const size_t _idx);
-			
-			
-			const Tensor& get_component(const size_t _idx) const;
-			
-			
-			void set_component(const size_t _idx, const Tensor& _T);
-            
-            
-			Tensor get_core(const size_t _blockPos) const;
-            
-            
-			Tensor get_average_core() const;
-			
-            
-			TTTensor get_average_tt() const;
-			
-			
-			/** 
-			* @brief Gets the ranks of the TTNetwork.
-			* @return A vector containing the current ranks.
-			*/
-			std::vector<size_t> ranks() const;
-            
+
+
+            Tensor& component(const size_t _idx);
+
+
+            const Tensor& get_component(const size_t _idx) const;
+
+
+            void set_component(const size_t _idx, const Tensor& _T);
+
+
+            Tensor get_core(const size_t _blockPos) const;
+
+
+            Tensor get_average_core() const;
+
+
+            TTTensor get_average_tt() const;
+
+
+            /**
+            * @brief Gets the ranks of the TTNetwork.
+            * @return A vector containing the current ranks.
+            */
+            std::vector<size_t> ranks() const;
+
             ///@brief Return the number of components
             size_t num_components() const;
-			
-			
-			/** 
-			* @brief Gets the rank of a specific egde of the TTNetwork.
-			* @param _i Position of the edge in question.
-			* @return The current rank of edge _i.
-			*/
-			size_t rank(const size_t _i) const;
-			
-			
-			void move_core_left(const double _eps, const size_t _maxRank);
-		
-			void move_core_right(const double _eps, const size_t _maxRank);
-			
-			
-			/** 
-			* @brief Move the core to a new position.
-			*/
-			void move_core(const size_t _position, const double _eps=EPSILON, const size_t _maxRank=std::numeric_limits<size_t>::max());
+
+
+            /**
+            * @brief Gets the rank of a specific egde of the TTNetwork.
+            * @param _i Position of the edge in question.
+            * @return The current rank of edge _i.
+            */
+            size_t rank(const size_t _i) const;
+
+
+            void move_core_left(const double _eps, const size_t _maxRank);
+
+            void move_core_right(const double _eps, const size_t _maxRank);
+
+
+            /**
+            * @brief Move the core to a new position.
+            */
+            void move_core(const size_t _position, const double _eps=EPSILON, const size_t _maxRank=std::numeric_limits<size_t>::max());
             value_t move_core(const size_t _position, const size_t _maxRank);
             
             
@@ -143,12 +143,11 @@ namespace xerus { namespace internal {
 
             bool all_entries_valid() const;
 
-			value_t frob_norm() const;
+            value_t frob_norm() const;
 
-            
-			size_t dofs() const;
-			
-	};
+            size_t dofs() const;
+
+    };
 
     void stream_writer(std::ostream& _stream, const BlockTT &_obj, misc::FileFormat _format);
     void stream_reader(std::istream& _stream, BlockTT &_obj, const misc::FileFormat _format);
--- src/xerus/blockTT.cpp
+++ src/xerus/blockTT.cpp
@@ -1,20 +1,20 @@
 // Xerus - A General Purpose Tensor Library
-// Copyright (C) 2014-2018 Benjamin Huber and Sebastian Wolf. 
-// 
+// Copyright (C) 2014-2018 Benjamin Huber and Sebastian Wolf.
+//
 // Xerus is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License,
 // or (at your option) any later version.
-// 
+//
 // Xerus is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License
 // along with Xerus. If not, see <http://www.gnu.org/licenses/>.
 //
-// For further information on Xerus visit https://libXerus.org 
+// For further information on Xerus visit https://libXerus.org
 // or contact us at contact@libXerus.org.
 
 /**
@@ -36,14 +36,14 @@
 #include <xerus/misc/basicArraySupport.h>
 
 namespace xerus { namespace internal {
-	const Index BlockTT::left, BlockTT::right, BlockTT::ext, BlockTT::p, BlockTT::r1, BlockTT::r2;
-	/*- - - - - - - - - - - - - - - - - - - - - - - - - - Constructors - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-    
+    const Index BlockTT::left, BlockTT::right, BlockTT::ext, BlockTT::p, BlockTT::r1, BlockTT::r2;
+    /*- - - - - - - - - - - - - - - - - - - - - - - - - - Constructors - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
     BlockTT::BlockTT(const std::vector<size_t>& _dimensions, const std::vector<size_t>& _ranks, const size_t _blockPosition, const size_t _blockDim) :
         P(_blockDim),
-        corePosition(_blockPosition), 
-        blockPosition(_blockPosition), 
-        dimensions(_dimensions) 
+        corePosition(_blockPosition),
+        blockPosition(_blockPosition),
+        dimensions(_dimensions)
     {
         REQUIRE(_dimensions.size() == _ranks.size()+1, "_dimensions.size() != _ranks.size()+1");
         REQUIRE(_blockPosition < _dimensions.size(), "_blockPosition >= _dimensions.size()");
@@ -70,175 +70,175 @@ namespace xerus { namespace internal {
     }
 
     BlockTT::BlockTT(const TTTensor& _tttensor, const size_t _blockPosition, const size_t _blockDim) :
-        P(_blockDim), 
-        corePosition(_blockPosition), 
-        blockPosition(_blockPosition), 
-        dimensions(_tttensor.dimensions) 
+        P(_blockDim),
+        corePosition(_blockPosition),
+        blockPosition(_blockPosition),
+        dimensions(_tttensor.dimensions)
     {
         REQUIRE(_tttensor.canonicalized && _tttensor.corePosition == _blockPosition, "Invalid core Position");
         _tttensor.require_correct_format();
-        
+
         // Get components
         for(size_t i = 0; i < _tttensor.degree(); ++i) {
             components.push_back(_tttensor.get_component(i));
         }
-        
+
         // Create block
         components[_blockPosition](left, ext, p, right) = components[_blockPosition](left, ext, right)*Tensor::ones({_blockDim})(p);
     }
-    
+
     size_t BlockTT::degree() const {
         return components.size();
     }
-    
+
     std::vector<size_t> BlockTT::ranks() const {
-		std::vector<size_t> res;
-		res.reserve(degree()-1);
-		for (size_t n = 1; n < degree(); ++n) {
-			res.push_back(components[n].dimensions[0]);
-		}
-		return res;
-	}
+        std::vector<size_t> res;
+        res.reserve(degree()-1);
+        for (size_t n = 1; n < degree(); ++n) {
+            res.push_back(components[n].dimensions[0]);
+        }
+        return res;
+    }
 
     size_t BlockTT::num_components() const { return components.size(); }
-	
-	size_t BlockTT::rank(const size_t _idx) const {
+
+    size_t BlockTT::rank(const size_t _idx) const {
         REQUIRE(_idx+1 < degree(), "Illegal index " << _idx <<" in TTNetwork::component, as there are onyl " << degree() << " components.");
         return components[_idx+1].dimensions[0];
     }
-    
-    
+
+
     Tensor& BlockTT::component(const size_t _idx) {
         REQUIRE(_idx < degree(), "Illegal index " << _idx <<" in TTNetwork::component, as there are onyl " << degree() << " components.");
         return components[_idx];
     }
-			
-    
+
+
     const Tensor& BlockTT::get_component(const size_t _idx) const {
         REQUIRE(_idx < degree(), "Illegal index " << _idx <<" in TTNetwork::component, as there are onyl " << degree() << " components.");
         return components[_idx];
     }
-    
-    
+
+
     void BlockTT::set_component(const size_t _idx, const Tensor& _T) {
         REQUIRE(_idx == corePosition, "Illegal index " << _idx <<" as core position is " << corePosition << ".");
         REQUIRE(components[_idx].dimensions == _T.dimensions, "Invalid dimensions given: " << components[_idx].dimensions << " vs " << _T.dimensions);
         components[_idx] = _T;
     }
-    
-    
+
+
     Tensor BlockTT::get_core(const size_t _blockPos) const {
         REQUIRE(_blockPos < P, "IE");
         Tensor coreCmp = components[corePosition];
         coreCmp.fix_mode(2, _blockPos);
         return coreCmp;
     }
-    
-    
+
+
     Tensor BlockTT::get_average_core() const {
         Tensor coreCmp;
         coreCmp(left, ext, right) = (1.0/double(P))*components[corePosition](left, ext, p, right)*Tensor::ones({P})(p);
         return coreCmp;
     }
-    
-    
+
+
     TTTensor BlockTT::get_average_tt() const {
         TTTensor ttRep(dimensions);
-		
-		for(size_t i = 0; i < degree(); i++) {
-			if(i == corePosition) {
-				ttRep.set_component(i, get_average_core());
-			} else {
-				ttRep.set_component(i, get_component(i));
-			}
-		}
-		
-		return ttRep;
+
+        for(size_t i = 0; i < degree(); i++) {
+            if(i == corePosition) {
+                ttRep.set_component(i, get_average_core());
+            } else {
+                ttRep.set_component(i, get_component(i));
+            }
+        }
+
+        return ttRep;
     }
-            
-    
+
+
     value_t BlockTT::frob_norm() const {
         return components[corePosition].frob_norm();
     }
     value_t frob_norm(const BlockTT& _x) { return _x.frob_norm(); }
-    
-    
+
+
     size_t BlockTT::dofs() const {
         size_t numDofs = 0;
-        
+
         for(const auto& comp : components) {
             numDofs += comp.size;
         }
-        
+
         for(const auto r : ranks()) {
             numDofs -= r*r;
         }
-        
+
 
         REQUIRE(corePosition == 0, "IE");
         numDofs -= components[0].size - misc::sqr(components[1].dimensions[0]);
-        
+
         return numDofs;
     }
-    
-    
-	void BlockTT::move_core_left(const double _eps, const size_t _maxRank) {
-		REQUIRE(corePosition > 0, "Can't move core left from position " << corePosition);
-		
-		if(P == 1) {
-			Tensor Q, R;
-			
-			(R(left, p, r1), Q(r1, ext, right)) = RQ(components[corePosition](left, ext, p, right));
-			
-			components[corePosition] = Q;
-			components[corePosition-1](left, ext, p, right) = components[corePosition-1](left, ext, r1)*R(r1, p, right);
-		} else {
-			Tensor U, S, V;
-			
-			(U(left, p, r1), S(r1, r2), V(r2, ext, right)) = SVD(components[corePosition](left, ext, p, right), _maxRank, _eps);
-			components[corePosition] = V;
-			components[corePosition-1](left, ext, p, right) = components[corePosition-1](left, ext, r1)*U(r1, p, r2)*S(r2, right);
-		}
-		corePosition--;
-		blockPosition--;
+
+
+    void BlockTT::move_core_left(const double _eps, const size_t _maxRank) {
+        REQUIRE(corePosition > 0, "Can't move core left from position " << corePosition);
+
+        if(P == 1) {
+            Tensor Q, R;
+
+            (R(left, p, r1), Q(r1, ext, right)) = RQ(components[corePosition](left, ext, p, right));
+
+            components[corePosition] = Q;
+            components[corePosition-1](left, ext, p, right) = components[corePosition-1](left, ext, r1)*R(r1, p, right);
+        } else {
+            Tensor U, S, V;
+
+            (U(left, p, r1), S(r1, r2), V(r2, ext, right)) = SVD(components[corePosition](left, ext, p, right), _maxRank, _eps);
+            components[corePosition] = V;
+            components[corePosition-1](left, ext, p, right) = components[corePosition-1](left, ext, r1)*U(r1, p, r2)*S(r2, right);
+        }
+        corePosition--;
+        blockPosition--;
     }
-    
-    
+
+
     void BlockTT::move_core_right(const double _eps, const size_t _maxRank) {
-		REQUIRE(corePosition+1 < degree(), "Can't move core right from position " << corePosition);
-			
-		if(P == 1) {
-			Tensor Q, R;
-			
-			(Q(left, ext, r1), R(r1, p, right)) = QR(components[corePosition](left, ext, p, right));
-			components[corePosition] = Q;
-			components[corePosition+1](left, ext, p, right) = R(left, p, r1)*components[corePosition+1](r1, ext, right);
-		} else {
-			Tensor U, S, V;
-			
-			(U(left, ext, r1), S(r1, r2), V(r2, p, right)) = SVD(components[corePosition](left, ext, p, right), _maxRank, _eps);
-			components[corePosition] = U;
-			components[corePosition+1](left, ext, p, right) = S(left, r1)*V(r1, p, r2)*components[corePosition+1](r2, ext, right);
-		}
-		corePosition++;
-		blockPosition++;
+        REQUIRE(corePosition+1 < degree(), "Can't move core right from position " << corePosition);
+
+        if(P == 1) {
+            Tensor Q, R;
+
+            (Q(left, ext, r1), R(r1, p, right)) = QR(components[corePosition](left, ext, p, right));
+            components[corePosition] = Q;
+            components[corePosition+1](left, ext, p, right) = R(left, p, r1)*components[corePosition+1](r1, ext, right);
+        } else {
+            Tensor U, S, V;
+
+            (U(left, ext, r1), S(r1, r2), V(r2, p, right)) = SVD(components[corePosition](left, ext, p, right), _maxRank, _eps);
+            components[corePosition] = U;
+            components[corePosition+1](left, ext, p, right) = S(left, r1)*V(r1, p, r2)*components[corePosition+1](r2, ext, right);
+        }
+        corePosition++;
+        blockPosition++;
     }
-	
-	
-	void BlockTT::move_core(const size_t _position, const double _eps, const size_t _maxRank) {
+
+
+    void BlockTT::move_core(const size_t _position, const double _eps, const size_t _maxRank) {
         REQUIRE(_position < degree(), "Invalid new core position " << _position);
-		
+
         while(corePosition < _position) {
             move_core_right(_eps, _maxRank);
         }
-        
+
         while(corePosition > _position) {
             move_core_left(_eps, _maxRank);
         }
     }
-    
+
     value_t BlockTT::move_core(const size_t _position, const size_t _maxRank) {
-		REQUIRE(_maxRank > 0, "maxRank must be larger than zero.");
+        REQUIRE(_maxRank > 0, "maxRank must be larger than zero.");
         REQUIRE(_position < degree(), "IE");
         Tensor U, S, V;
         value_t ret = 0; // corePosition == _position
@@ -265,13 +265,13 @@ namespace xerus { namespace internal {
 
         return ret;
     }
-    
+
     void BlockTT::average_core() {
         Tensor coreCmp;
         coreCmp(left, ext, right) = (1.0/double(P))*components[corePosition](left, ext, p, right)*Tensor::ones({P})(p);
         components[corePosition](left, ext, p, right) = coreCmp(left, ext, right)*Tensor::ones({P})(p);
     }
-    
+
 
     void stream_writer(std::ostream& _stream, const BlockTT &_obj, misc::FileFormat _format) {
         if(_format == misc::FileFormat::TSV) {
@@ -279,15 +279,15 @@ namespace xerus { namespace internal {
         }
         // storage version number
         xerus::misc::write_to_stream<size_t>(_stream, 1, _format);
-        
+
         xerus::misc::write_to_stream<size_t>(_stream, _obj.P, _format);
         xerus::misc::write_to_stream<size_t>(_stream, _obj.corePosition, _format);
         xerus::misc::write_to_stream<size_t>(_stream, _obj.blockPosition, _format);
         xerus::misc::write_to_stream<std::vector<size_t>>(_stream, _obj.dimensions, _format);
         xerus::misc::write_to_stream<std::vector<xerus::Tensor>>(_stream, _obj.components, _format);
     }
-    
-    
+
+
     void stream_reader(std::istream& _stream, BlockTT &_obj, const misc::FileFormat _format) {
         size_t ver = xerus::misc::read_from_stream<size_t>(_stream, _format);
         REQUIRE(ver == 1, "Unknown stream version to open (" << ver << ")");
@@ -298,7 +298,7 @@ namespace xerus { namespace internal {
         _obj.dimensions = xerus::misc::read_from_stream<std::vector<size_t>>(_stream, _format);
         _obj.components = xerus::misc::read_from_stream<std::vector<Tensor>>(_stream, _format);
     }
-    
+
 
     bool BlockTT::all_entries_valid() const {
         for (const auto& comp : components) {
